6aeadfaeeee99ffd92ab6084e61dea367fccdd44
Uddercover <112282356+UdderlyImprobable@users.noreply.github.com>
2023-11-08 15:47:36 +0100

    Create the playRound function
    This function allows the user to play a round with the computer

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
 function getComputerChoice() {
   let x = Math.floor(Math.random()*100) + 1;
   if(x <= 34) {
-    return "Rock";
+    return "rock";
   } else if(x <= 67) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissors"
+    return "scissors"
   }
   
+}
+
+function playRound(playerSelection) {
+  computerSelection = getComputerChoice();
+  playerSelection = playerSelection.toLowerCase();
+
+    if (computerSelection === "rock" && playerSelection === "paper" || computerSelection === "scissors" && playerSelection === "rock" || computerSelection === "paper" && playerSelection === "scissors"){
+      return "You win!";
+    } else if (computerSelection === playerSelection) {
+      return "Stalemate!"
+    }
+    else if (computerSelection === "paper" && playerSelection === "rock" || computerSelection === "rock" && playerSelection === "scissors" || computerSelection === "scissors" && playerSelection === "paper"){
+      return "Loser :)";
+    } else {
+      console.log(computerSelection, playerSelection)
+    }
+}
+
+function game() {
+  let computer = 0;
+  let player = 0; 
+  for(let i = 0; i < 5; i++) {
+    let answer = playRound(prompt("Pick your weapon", ""));
+    console.log(answer);
+    if(answer === "You win!") {
+      player++;
+    }else if(answer === "Loser :)") {
+      computer++
+    }
+  }
+  return (computer > player)? "You Lose :(": "Winner!"
 }
