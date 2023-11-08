f464d2012526ef976c81a0e5e7dc03f4c76e25ee
Uddercover <112282356+UdderlyImprobable@users.noreply.github.com>
2023-11-08 16:07:56 +0100

    Modify the game function
    Two major modifications were made:
    Make the game continue until a player gets five wins and
    Make it so only wins and losses count as valid games.

--- script.js
+++ script.js
@@ -13,29 +13,31 @@ function playRound(playerSelection) {
   computerSelection = getComputerChoice();
   playerSelection = playerSelection.toLowerCase();
 
-    if (computerSelection === "rock" && playerSelection === "paper" || computerSelection === "scissors" && playerSelection === "rock" || computerSelection === "paper" && playerSelection === "scissors"){
+  if (computerSelection === "rock" && playerSelection === "paper" || computerSelection === "scissors" && playerSelection === "rock" || computerSelection === "paper" && playerSelection === "scissors"){
       return "You win!";
-    } else if (computerSelection === playerSelection) {
+  } else if (computerSelection === playerSelection) {
       return "Stalemate!"
-    }
-    else if (computerSelection === "paper" && playerSelection === "rock" || computerSelection === "rock" && playerSelection === "scissors" || computerSelection === "scissors" && playerSelection === "paper"){
+  } else if (computerSelection === "paper" && playerSelection === "rock" || computerSelection === "rock" && playerSelection === "scissors" || computerSelection === "scissors" && playerSelection === "paper"){
       return "Loser :)";
-    } else {
+  } else {
       console.log(computerSelection, playerSelection)
-    }
+  }
 }
 
 function game() {
   let computer = 0;
   let player = 0; 
-  for(let i = 0; i < 5; i++) {
+  for(let i = 0; i < 9; i++) {
     let answer = playRound(prompt("Pick your weapon", ""));
     console.log(answer);
     if(answer === "You win!") {
       player++;
     }else if(answer === "Loser :)") {
       computer++
+    } else { //make it so only wins and losses count as valid
+      i--;
     }
+
   }
-  return (computer > player)? "You Lose :(": "Winner!"
+  return (computer = 5 && computer > player)? "You Lose :(": "Winner!"
 }
